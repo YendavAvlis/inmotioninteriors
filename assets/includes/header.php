53d41246419a009afabe1557ab99b6f0d95ba537
Vadney <vadneysilva@hotmail.com>
2022-05-14 09:27:09 +0100

    meta tag

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -3,9 +3,14 @@
 
 <head>
     <meta charset="utf-8">
+    <meta http-equiv=”Refresh” content=”0;URL=https://www.inmotioninteriors.com” />
+
+
 
     <!--====== Title ======-->
-    <title><?php echo TITLE; ?></title>
+    <title>
+        <?php echo TITLE; ?>
+    </title>
 
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
